9b9088b6f9f850e45ec12432686a21e29b4e35d1
yunus <yycan77@hotmail.com>
2019-03-16 20:50:22 +0300

    Optimized and finalized A*

--- hw1.py
+++ hw1.py
@@ -1,64 +1,143 @@
+from queue import PriorityQueue
+import heapq
+import timeit
+from copy import deepcopy
+outDict = {}
 # Read and parse the file => (coordX,coordY)
-def readFile(input,output):
+
+
+class Node:
+    def __init__(self, state, emptyCoord, g):
+        global outDict
+        self.state = state
+        self.deleted = 0
+        self.emptyCoord = emptyCoord
+        self.g = g
+        self.pathCost = calculateH(self.state, outDict) + self.g
+
+    def __eq__(self, other):
+        return self.pathCost == other.pathCost
+
+    def __lt__(self, other):
+        return self.pathCost < other.pathCost
+
+
+def readFile(outDict):
     out = 0
     coord = ()
+    input = ()
+    output = ()
+    numLineInp = 0
     with open('input.txt') as f:
         lines = f.readlines()
-        for i,line in enumerate(lines):
+        for i, line in enumerate(lines):
             stripped = line.strip()
             if not stripped:
                 out = 1
+                numLineInp += 1
                 continue
             if not out:
-                inner = []
-                for j,num in enumerate(stripped.split()):
+                for j, num in enumerate(stripped.split()):
                     if num == '0':
-                        coord = (i,j)
-                    inner.append(int(num))
-                input.append(inner)
+                        coord = (i, j)
+                    input += (int(num),)
+                numLineInp += 1
             else:
-                output.append([int(n) for n in stripped.split()])
-    return coord
+                for j, num in enumerate(stripped.split()):
+                    output += (int(num),)
+                    outDict[int(num)] = (i-numLineInp, j)
+    return (input,output,coord)
 # Generate possible actions(u,d,l,r) w.r.t coordinates => [string]
+
+
 def generateActions(emptyCoord):
     actions = []
-    i,j = emptyCoord
-    if i == 0:
+    i, j = emptyCoord
+    if j == 0:
         actions.append('r')
-    elif i == 1:
-        actions.extend(['l','r'])
+    elif j == 1:
+        actions.extend(['l', 'r'])
     else:
         actions.append('l')
-    if j == 0:
+    if i == 0:
         actions.append('d')
-    elif j == 1:
-        actions.extend(['u','d'])
+    elif i == 1:
+        actions.extend(['u', 'd'])
     else:
         actions.append('u')
 
     return actions
 # Apply the action => (newCoordx,newCoordy,newState)
-def applyAction(state,action,emptyCoord):
-    i, j = emptyCoord
+def applyAction(node, action):
+    i, j = node.emptyCoord
     if not action:
-        return (i,j,state)
-    newState = [[i for i in row] for row in state]
+        return node
+    newState = list(node.state)
     if action == 'd':
-        newState[i+1][j],newState[i][j] = newState[i][j],newState[i+1][j]
-        return (i+1,j,newState)
+        newState[(i+1)*3 +j ], newState[i*3 +j] = newState[i*3 +j], newState[(i+1)*3 +j ]
+        return Node(tuple(newState), (i + 1, j), node.g + 1)
     elif action == 'u':
-        newState[i-1][j],newState[i][j] = newState[i][j],newState[i-1][j]
-        return (i-1,j,newState)
+        newState[(i-1)*3+ j], newState[i*3 +j] = newState[i*3 +j], newState[(i-1)*3+ j]
+        return Node(tuple(newState), (i - 1, j), node.g + 1)
     elif action == 'r':
-        newState[i][j+1],newState[i][j] = newState[i][j],newState[i][j+1]
-        return (i,j+1,newState)
+        newState[i*3 + j+1], newState[i*3 +j] = newState[i*3 +j], newState[i*3 + j+1]
+        return Node(tuple(newState), (i, j + 1), node.g + 1)
     elif action == 'l':
-        newState[i][j-1],newState[i][j] = newState[i][j],newState[i][j-1]
-        return (i,j-1,newState)
-    return  (i,j,state)
+        newState[i*3 + j-1], newState[i*3 +j] = newState[i*3 +j], newState[i*3 + j-1]
+        return Node(tuple(newState), (i, j - 1), node.g + 1)
+    return node
+#Calculate heuristics
+def calculateH(state, outDict):
+    heur = 0
+    for idx, num in enumerate(state):
+        i = idx // 3
+        j = idx % 3
+        if num != 0:
+            heur += abs(outDict[num][0] - i) + abs(outDict[num][1] - j)
+    return heur
+
+
+def stateInFrontier(state, frontierDict):
+   return frontierDict.get(state)
+
+def stateInExplored(state, explored):
+   return explored.get(state)
+
+def A_star(initNode):
+    frontier = []
+    explored = {}
+    frontierDict = {}
+    heapq.heappush(frontier, initNode)
+    while frontier:
+        node = heapq.heappop(frontier)
+        if node.deleted:
+            continue
+        if node.pathCost == node.g:
+            print(node.state,iter)
+            return
+        explored[node.state] = 1
+        for action in generateActions(node.emptyCoord):
+            child = applyAction(node, action)
+            frontierRef = stateInFrontier(child.state, frontierDict)
+            if not (frontierRef or stateInExplored(child.state, explored)):
+                heapq.heappush(frontier, child)
+                frontierDict[child.state] = child
+            elif frontierRef and frontierRef.pathCost > child.pathCost:
+                frontierRef.deleted = 1
+                heapq.heappush(frontier,child)
+                frontierDict[child.state] = child
+    print("nope",iter)
+
+    return None
+
+
 def main():
-    input = []
-    output = []
-    emptyCoord = readFile(input, output)
+    global outDict
+    start = timeit.default_timer()
+    input,output,emptyCoord = readFile(outDict)
+    A_star(Node(input, emptyCoord, 0))
+    stop = timeit.default_timer()
+    print('Time: ', stop - start)
+
 if __name__ == '__main__':
     main()
